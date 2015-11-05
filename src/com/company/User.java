30763bd4474a8d0314542bd5375d756934b4b994
cpoakley1432 <cpoakley1432@gmail.com>
2015-11-05 14:03:15 -0500

    initial import

---
+++ src/com/company/User.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by cameronoakley on 11/5/15.
+ */
+public class User {
+    int id;
+    String username;
+    String password;
+    String url;
+
+}
---
+++ src/com/company/Workout.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by cameronoakley on 11/5/15.
+ */
+public class Workout {
+    int id
+}
