102e8e891e34bec1f1f8b07b199095e623191ca7
Brandon Jones <bdjones2410@gmail.com>
2015-11-05 17:47:40 -0500

    working on this

--- brandon.js
+++ brandon.js
@@ -4,38 +4,17 @@ $('form').on('click','.mainLogIn', function(e){
         e.preventDefault();
         var user = $(this).siblings('input[name="username"]').val();
         var pwd = $(this).siblings('input[name="pass"]').val();
-        console.log(pwd + " " + user);
-        data = {
-          username: user,
-          password: pwd,
-        }
-        console.log(JSON.stringify(data));
         $.ajax({
           url:'/login',
           method:'POST',
-          data: JSON.stringify(data),
-          success:function(returndata){
-            console.log("success: " + returndata);
+          data: {username: user, password:pwd},
+          success:function(stuff){
+            console.log("success: " + stuff);
 
           },
-          failure:function(returndata){
-            console.log("Fail: " +returndata);
+          failure:function(stuff){
+            console.log("Fail: " +stuff);
           }
 
         })
 });
-
-$.ajax({
-  url:'/login',
-  method:'GET',
-  success:function(data){
-    console.log("Success: " + data);
-  },
-  failure:function(data){
-    console.log("FAilure: "+ data);
-  }
-});
-
-
-
-// /loggedin
