9118522267df5a4b9e38482694a1e98bb3617951
cpoakley1432 <cpoakley1432@gmail.com>
2015-11-05 14:19:56 -0500

    insert user and table

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.sql.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void createTables(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY , name VARCHAR , password VARCHAR , url VARCHAR)");
+    }
+
+    public static void insertUser (Connection conn , String username, String password , String url) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO users VALUES (NULL , ? , ? , ?)");
+        stmt.setString(1, username);
+        stmt.setString(2, password);
+        stmt.setString(3 , url);
+        stmt.execute();
+    }
+
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
 
     }
 }
--- src/com/company/Workout.java
+++ src/com/company/Workout.java
@@ -4,5 +4,5 @@ package com.company;
  * Created by cameronoakley on 11/5/15.
  */
 public class Workout {
-    int id
+    int id;
 }
