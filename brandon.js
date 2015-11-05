7a890921eacf80ca1b9c5f70582275ca9de77735
Brandon Jones <bdjones2410@gmail.com>
2015-11-05 16:47:26 -0500

    playing with log in

---
+++ brandon.js
@@ -0,0 +1,30 @@
+
+
+$('form').on('click','.mainLogIn', function(e){
+        e.preventDefault();
+        var user = $(this).siblings('input[name="username"]').val();
+        var pwd = $(this).siblings('input[name="pass"]').val();
+        console.log(pwd + " " + user);
+        data = {
+          username: user,
+          password: pwd,
+        }
+        console.log(JSON.stringify(data));
+        $.ajax({
+          url:'/login',
+          method:'POST',
+          data: JSON.stringify(data),
+          success:function(returndata){
+            console.log("success: " + returndata);
+
+          },
+          failure:function(returndata){
+            console.log("Fail: " +returndata);
+          }
+
+        })
+});
+
+
+
+// /loggedin
