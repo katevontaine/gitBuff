05c7bc9fafa0ed37ca05905b53b1ed4ad3c3ae03
cpoakley1432 <cpoakley1432@gmail.com>
2015-11-08 12:57:03 -0500

    insert note

---
+++ src/com/company/Note.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by cameronoakley on 11/8/15.
+ */
+public class Note {
+    int id;
+    LocalDateTime noteDate;
+    String text;
+}
